b997236cd8d89d5453bd7cdbeb09944494a1ecf6
saman Hoo <samanhoo121@gmail.com>
2026-07-28 22:30:48 +0330

    Add AI router module

---
+++ src/core/ai_router.py
@@ -0,0 +1,14 @@
+from core.ai_client import AIClient
+
+
+class AIRouter:
+
+    def __init__(self):
+        self.providers = {
+            "default": AIClient()
+        }
+
+    def generate(self, prompt):
+        provider = self.providers["default"]
+
+        return provider.generate(prompt)
